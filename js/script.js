e26fd7c7258ccf347f718b5697d13f0cffbc1545
Kirill Samylin <kirill-itb@yandex.ru>
2020-08-03 16:56:38 +0400

    way

---
+++ js/script.js
@@ -0,0 +1,35 @@
+//
+//                                                                                                   00000
+//                                                                                                  
+//             00000000000000       00          00    000          000            000           00         0000
+//             00          00        00        00     00 00      00 00           00 00          00        00 00
+//             00          00         00      00      00  00    00  00          00   00         00       00  00
+//             00          00          00    99       00   00  00   00         00     00        00      00   00
+//             00          00           00  00        00    0000    00        00000000000       00     00    00
+//          00000000000000000000          000         00            00       0000000000000      00    00     00
+//          00000000000000000000          00          00            00      00           00     00   00      00
+//          00                00         00           00            00     00             00    00  00       00
+//          00                00        00            00            00    00               00   00 00        00
+//          00                00       00             00            00   00                 00  0000         00
+//                     
+//                                    
+//                                        000000               000            000            000
+//                                      000   0000            00 00           00 00        00 00
+//                                     00                    00   00          00  00      00  00
+//                                    00                    00     00         00   00    00   00
+//                                    00                   00       00        00    00  00    00
+//                                    00                  0000000000000       00     0000     00
+//                                    00                 000000000000000      00              00
+//                                    00                00             00     00              00
+//                                     00              00               00    00              00
+//                                      000    000    00                 00   00              00
+//                                        0000000    00                   00  00              00
+//
+//
+//
+//
+//
+//
+//
+//
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('m K=C.D(\'.K\');m S=C.D(\'.q\');m W=C.D(\'.u\');j 9(t){w 1i t==="t"};j 16(u){k L=\'\';u.p((3)=>{L+=15.1g(3).1f(/"/g,"")+"\\n"});W.U=L};j q(V){S.U=V};j F(4){k Q=[];k d=[];k h=[];j E(3){w{y:4[3.6]?.[3.5+1],6:3.6,5:3.5+1,8:(9(4[3.6+1]?.[3.5+1]))?(4[3.6+1]?.[3.5+1]):s,7:(9(4[3.6]?.[3.5+2]))?(4[3.6]?.[3.5+2]):s,}};j G(3){w{y:4[3.6+1]?.[3.5],6:3.6+1,5:3.5,8:(9(4[3.6+2]?.[3.5]))?(4[3.6+2]?.[3.5]):s,7:(9(4[3.6+1]?.[3.5+1]))?(4[3.6+1]?.[3.5+1]):s,}};j T(3,o){k 7=[];k 8=[];f(9(3.8)&&9(3.7)){7=h[o].z();8=h[o].z();7.c(E(3));8.c(G(3));h[o]=7;h.c(8)};f(!9(3.8)&&9(3.7)){7=h[o].z();7.c(E(3));h[o]=7};f(9(3.8)&&!9(3.7)){8=h[o].z();8.c(G(3));h[o]=8}};4.p((F,l)=>{F.p((H,i)=>{f(9(4[l]?.[i])){Q.c({y:H,6:l,5:i,8:(9(4[l+1]?.[i]))?(4[l+1]?.[i]):s,7:(9(4[l]?.[i+1]))?(4[l]?.[i+1]):s,})}})});Q.p((3)=>{f(d.B===0){k 7=[];k 8=[];f(9(3.8)){8.c(3);8.c(G(3));d.c(8)};f(9(3.7)){7.c(3);7.c(E(3));d.c(7)}}I{h=d.z();1j(k i=0;i<d.B;i++){f(d[i].1p((v)=>(v.6===3.6&&v.5===3.5))){T(3,i)}};d=h}});d.p((4,l)=>{d[l].J=4.1k((X,P)=>(X+P.y),0)});m O=d.1m((a,b)=>(a.J-b.J))[0];m u=[];4.p((r,6)=>{m R=[];r.p((P,5)=>{m H=O.1l((v)=>(v.6===6&&v.5===5))?.y??\'*\';R.c(H)});u.c(R)});16(u);q(\' \'+O.J)};j 14(r){f(r.B>A||r.B===0){q(\'Ошибка! На вход подается матрица размерностью N x M (1<=N<=A, 1<=M<=A).\');w}I{j 10(11){w 11.12((t)=>t>=0&&t<=Z)}f(r.12((4)=>(1n.1o(4)&&10(4)))){F(r)}I{q(\'Ошибка! На вход подается матрица размерностью N x M (1<=N<=A, 1<=M<=A) из цифр от 0 до Z\')}}};j Y(){m 13=C.D(\'.4\');18{m 17=13.19;14(15.1b(17))}1h(e){f(e 1c 1d){q(\'Ошибка! Введите массив!\')}I{q(\'Ошибка! Введите массив!\')}}};K.1e(\'1a\',Y);',62,88,'|||line|arr|positionRight|positionDown|right|down|isNaN|||push|sumWay||if||copyArr||function|let|index|const||pos|forEach|mgs|array|false|number|route|obj|return||position|concat|20|length|document|querySelector|rightObj|steps|downObj|step|else|sum|start|text|||cheapWay|item|wayArr|routeSteps|mgsParagraph|goWay|textContent|str|msgRoute|cost|checkInput|100|checksCharacters|numbers|every|inputText|checkArray|JSON|answerRoute|inputArray|try|value|click|parse|instanceof|SyntaxError|addEventListener|replace|stringify|catch|typeof|for|reduce|find|sort|Array|isArray|some'.split('|'),0,{}))
